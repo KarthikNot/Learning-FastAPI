c75676c4053f3781e6de5cd1fe3ab8467ea97ff1
Karthikeyan's Mac <anumallakarthikeyan03@gmail.com>
2025-07-01 12:19:26 +0530

    with exceptions and status codes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from schemas import Blog
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -17,7 +17,7 @@ app = FastAPI()
 def health():
     return "App is healthy"
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def createBlog(request : Blog, db : Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
@@ -25,12 +25,14 @@ def createBlog(request : Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
+@app.get('/blog', status_code = status.HTTP_200_OK)
 def getBlogs(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def getBlogById(id : int, db : Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def getBlogById(id : int, res : Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found.")
     return blog
